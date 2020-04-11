a0843e3d2c25347098a88a282c4b90715766665a
Vargese Antony <VKalist@slb.com>
2020-04-11 23:03:07 +0530

    Initial changes

---
+++ BooksApi/Controllers/BooksController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BooksApi.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BooksApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBooksRepository _booksRepository;
+        public BooksController(IBooksRepository booksRepository)
+        {
+            _booksRepository = booksRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBooks()
+        {
+            var books = await _booksRepository.GetBooksAsync();
+            return Ok(books);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetBookById(Guid id)
+        {
+            var book = await _booksRepository.GetBookAsync(id);
+            if(book == null)
+            {
+                return NotFound();
+            }
+            return Ok(book);
+        }
+    }
+}
---
+++ BooksApi/Services/IBooksRepository.cs
@@ -0,0 +1,19 @@
+﻿using BooksApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksApi.Services
+{
+    public interface IBooksRepository
+    {
+        //IEnumerable<Entities.Book> GetBooks();
+
+        //Entities.Book GetBook(Guid Id);
+
+        Task<IEnumerable<Entities.Book>> GetBooksAsync();
+
+        Task<Entities.Book> GetBookAsync(Guid Id);
+    }
+}
